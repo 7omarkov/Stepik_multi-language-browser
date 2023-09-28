d8f2f07723c2102850c49a93541c0c1757fd7a24
Oleg Markov <7omarkov@gmail.com>
2023-09-28 19:34:30 +0300

    Class structure removed

--- test_items.py
+++ test_items.py
@@ -1,15 +1,15 @@
 import time
 from selenium.webdriver.common.by import By
 
-class Test_selenium1py_pythonanywhere():
 
-    def test_add_item_button_exist(self, browser):
-        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
-        time.sleep(3)
+def test_add_item_button_exist(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
-        add_item_button = browser.find_elements(By.XPATH, "//button[contains(@class,'add-to-basket')]")
+    time.sleep(2)
 
-        assert add_item_button, "Add to basket button not found"
+    add_item_button = browser.find_elements(By.XPATH, "//button[contains(@class,'add-to-basket')]")
 
-        time.sleep(3)
+    assert add_item_button, "Add to basket button not found"
+
+    time.sleep(1)
